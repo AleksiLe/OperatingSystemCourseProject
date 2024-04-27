7c85f5bede299e3b96df8e33d75722a3e73bae3d
AleksiLe <aleksi.levanen@gmail.com>
2024-04-27 15:50:06 +0300

    Reverse done (still few more tests)

--- Reverse/reverse.c
+++ Reverse/reverse.c
@@ -9,6 +9,7 @@ Additional help from:
 GitHub CoPilot
 https://stackoverflow.com/questions/3495092/read-from-file-or-stdin
 https://solarianprogrammer.com/2019/04/03/c-programming-read-file-lines-fgets-getline-implement-portable-getline/
+https://stackoverflow.com/questions/29522264/correct-way-to-use-malloc-and-free-with-linked-lists
 
 For testing purposes copy pasta:
 valgrind --leak-check=full ./reverse input.txt
@@ -28,13 +29,15 @@ struct Node
     struct Node *prev;
 };
 
+// Read from input file and append words to linked list
+// Return the end of the linked list
+// Because of this the linked list is efficient to reverse
 struct Node *readToLinkedList(FILE *inputFile)
 {
     char *line = NULL;
     size_t len = 0;
     int isStdin = (inputFile == stdin);
     struct Node *head = NULL;
-    struct Node *end = NULL;
 
     while ((getline(&line, &len, inputFile)) != -1)
     {   
@@ -46,16 +49,75 @@ struct Node *readToLinkedList(FILE *inputFile)
         {
             line[strlen(line) - 1] = '\0';
         }
-        printf("%s\n", line); // Print line to terminal (for testing purposes)
-        //  struct Node *newNode = malloc(sizeof(struct Node));
+        //  printf("%s\n", line); // Print line to terminal (for testing purposes)
+        struct Node *newNode = malloc(sizeof(struct Node));
+        if (newNode == NULL) // Check if malloc failed
+        {
+            fprintf(stderr, "malloc failed\n");
+            free(newNode);
+            exit(1);
+        }
+        newNode->line = strdup(line);
+        if (newNode->line == NULL) // Check if strdup failed
+        {
+            fprintf(stderr, "malloc(strderr) failed\n");
+            free(newNode->line);
+            exit(1);
+        }
+        if (head == NULL)
+        {
+            newNode->prev = NULL;
+            newNode->next = NULL;
+            head = newNode;
+        } else
+        {
+            newNode->prev = head;
+            newNode->next = NULL;
+            head->next = newNode;
+            head = newNode;
+        }
     }
     free(line); 
     line = NULL; 
-    return end;
+    return head;
+}
+
+void readFromLinkedListToFile(struct Node *end, FILE *outputFile)
+{
+    while (end != NULL)
+    {
+        fprintf(outputFile, "%s\n", end->line);
+        end = end->prev;
+    }
+    return;
+}
+
+void readFromLinkedListToTerminal(struct Node *end)
+{
+    while (end != NULL)
+    {
+        printf("%s\n", end->line);
+        end = end->prev;
+    }
+    return;
+}
+
+void freeLinkedList(struct Node *end)
+{
+    struct Node *temp;
+    while (end != NULL)
+    {
+        temp = end;
+        end = end->prev;
+        free(temp->line);
+        free(temp);
+    }
+    return;
 }
 
 int main(int argc, char *argv[])
 {
+    struct Node *end = NULL;
     if (argc > 3) // Check correct amount of arguments
     {
         fprintf(stderr, "Usage: reverse <input> <output>\n");
@@ -70,11 +132,28 @@ int main(int argc, char *argv[])
         }
 
         // Read from input file and append words to linked list
-        struct Node *end = readToLinkedList(inputFile);
+        end = readToLinkedList(inputFile);
         fclose(inputFile);
+
+        if (argv[2] != NULL) // Write to output file
+        {
+            FILE *outputFile = fopen(argv[2], "w");
+            if (outputFile == NULL) // Check if file exists
+            {
+                fprintf(stderr, "reverse: cannot open file '%s'\n", argv[2]);
+                exit(1);
+            }
+            readFromLinkedListToFile(end, outputFile);
+            fclose(outputFile);
+        } else // Print to terminal
+        {
+            readFromLinkedListToTerminal(end);
+        }
     } else if (argc == 1) // Read from standard input and print to terminal
     {
-        struct Node *end = readToLinkedList(stdin);
+        end = readToLinkedList(stdin);
+        readFromLinkedListToTerminal(end);
     }
+    freeLinkedList(end);
     return 0;
 }
